e6e0d098db67fa94036a90ab0b626b02c2bad391
RosmeryGallardo <rcedenog@uoc.edu>
2025-04-17 21:03:19 +0200

    Todofinaly

--- PEC3_Ej4/app.ts
+++ PEC3_Ej4/app.ts
@@ -1,4 +1,7 @@
 import { TodoController } from './controllers/todo.controller';
 import { TodoService } from './services/todo.service';
 import { TodoView } from './views/todo.views';
+import 'style.css';  
+import 'index.html';
+
 const app = new TodoController(new TodoService(), new TodoView());
